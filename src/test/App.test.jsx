61fa5b44f7f35aff4acb7c66f1ccfe7b6811cbf4
habderemane <hananeabderemane@gmail.com>
2026-07-18 22:05:07 +0000

    Test: trigger pipeline failure notification

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -5,7 +5,7 @@ import App from '../App'
 describe('App', () => {
   it('affiche le titre', () => {
     render(<App />)
-    expect(screen.getByText('Company DevOps')).toBeInTheDocument()
+    expect(screen.getByText('Company DevOpsXXX')).toBeInTheDocument()
   })
 
   it('affiche la liste des utilisateurs après chargement', async () => {
